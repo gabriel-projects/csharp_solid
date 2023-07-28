297474fdc18d09050a8decfab7d62a0940fcb22e
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-07-28 19:32:12 -0300

    isp

---
+++ ISP - Interface Segregation Principle (Princípio da Segregação de Interfaces)/ClassLibrary/Animais/Animais.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Animais
+{
+    public class Animais
+    {
+        public Animais()
+        {
+            
+        }
+    }
+
+    public interface IAve
+    {
+        void Bicar();
+        void Voar();
+    }
+
+
+    public class Gaviao : IAve
+    {
+        public void Bicar()
+        {
+            //BICAR
+        }
+
+        public void Voar()
+        {
+            //VOAR
+        }
+    }
+
+    public class Pinguin : IAve
+    {
+        public void Bicar()
+        {
+            //bicar
+        }
+
+        public void Voar()
+        {
+            //não faz sentido
+        }
+    }
+}
